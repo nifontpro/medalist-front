a6f5b44499708d5af6e9c2d13d4a4ac37386614b
Zykov Artem <skaterpunisher@gmail.com>
2023-03-30 15:02:41 +0300

    fix TreeNode

--- src/app/components/Sidebar/TreeNode/TreeNode.tsx
+++ src/app/components/Sidebar/TreeNode/TreeNode.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { TreeNodeProps } from './TreeNode.props';
 import Tree from '../Tree/Tree';
 import styles from './TreeNode.module.scss';
@@ -6,91 +8,105 @@ import TreeItem, { TreeItemProps, treeItemClasses } from '@mui/lab/TreeItem';
 import { SvgIconProps } from '@mui/material/SvgIcon';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import EditIcon from '@/icons/edit.svg';
 import { ImageDefault } from '@/ui/ImageDefault/ImageDefault';
 
-declare module 'react' {
-  interface CSSProperties {
-    '--tree-view-color'?: string;
-    '--tree-view-bg-color'?: string;
-  }
-}
+// declare module 'react' {
+//   interface CSSProperties {
+//     '--tree-view-color'?: string;
+//     '--tree-view-bg-color'?: string;
+//   }
+// }
 
-type StyledTreeItemProps = TreeItemProps & {
-  bgColor?: string;
-  color?: string;
-  labelIcon: React.ElementType<SvgIconProps>;
-  labelInfo?: string;
-  labelText: string;
-};
+// type StyledTreeItemProps = TreeItemProps & {
+//   bgColor?: string;
+//   color?: string;
+//   labelIcon: React.ElementType<SvgIconProps>;
+//   labelInfo?: string;
+//   labelText: string;
+// };
 
-const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
-  color: theme.palette.text.secondary,
-  [`& .${treeItemClasses.content}`]: {
-    color: theme.palette.text.secondary,
-    borderTopRightRadius: theme.spacing(2),
-    borderBottomRightRadius: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-    fontWeight: theme.typography.fontWeightMedium,
-    '&.Mui-expanded': {
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-    '&:hover': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
-      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
-      color: 'var(--tree-view-color)',
-    },
-    [`& .${treeItemClasses.label}`]: {
-      fontWeight: 'inherit',
-      color: 'inherit',
-    },
-  },
-  [`& .${treeItemClasses.group}`]: {
-    marginLeft: 0,
-    [`& .${treeItemClasses.content}`]: {
-      paddingLeft: theme.spacing(2),
-    },
-  },
-}));
+// const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
+//   color: theme.palette.text.secondary,
+//   [`& .${treeItemClasses.content}`]: {
+//     color: theme.palette.text.secondary,
+//     borderTopRightRadius: theme.spacing(2),
+//     borderBottomRightRadius: theme.spacing(2),
+//     paddingRight: theme.spacing(1),
+//     fontWeight: theme.typography.fontWeightMedium,
+//     '&.Mui-expanded': {
+//       fontWeight: theme.typography.fontWeightRegular,
+//     },
+//     '&:hover': {
+//       backgroundColor: theme.palette.action.hover,
+//     },
+//     '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+//       backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
+//       color: 'var(--tree-view-color)',
+//     },
+//     [`& .${treeItemClasses.label}`]: {
+//       fontWeight: 'inherit',
+//       color: 'inherit',
+//     },
+//   },
+//   [`& .${treeItemClasses.group}`]: {
+//     marginLeft: 0,
+//     [`& .${treeItemClasses.content}`]: {
+//       paddingLeft: theme.spacing(2),
+//     },
+//   },
+// }));
 
+// function StyledTreeItem(props: StyledTreeItemProps) {
+//   const {
+//     bgColor,
+//     color,
+//     labelIcon: LabelIcon,
+//     labelInfo,
+//     labelText,
+//     ...other
+//   } = props;
 
-function StyledTreeItem(props: StyledTreeItemProps) {
-  const {
-    bgColor,
-    color,
-    labelIcon: LabelIcon,
-    labelInfo,
-    labelText,
-    ...other
-  } = props;
-
-  return (
-    <StyledTreeItemRoot
-      label={
-        <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
-          <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
-          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
-            {labelText}
-          </Typography>
-          <Typography variant="caption" color="inherit">
-            {labelInfo}
-          </Typography>
-        </Box>
-      }
-      style={{
-        '--tree-view-color': color,
-        '--tree-view-bg-color': bgColor,
-      }}
-      {...other}
-    />
-  );
-}
+//   return (
+//     <StyledTreeItemRoot
+//       label={
+//         <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+//           <Box component={LabelIcon} color='inherit' sx={{ mr: 1 }} />
+//           <Typography
+//             variant='body2'
+//             sx={{ fontWeight: 'inherit', flexGrow: 1 }}
+//           >
+//             {labelText}
+//           </Typography>
+//           <Typography variant='caption' color='inherit'>
+//             {labelInfo}
+//           </Typography>
+//         </Box>
+//       }
+//       style={{
+//         '--tree-view-color': color,
+//         '--tree-view-bg-color': bgColor,
+//       }}
+//       {...other}
+//     />
+//   );
+// }
 
 const TreeNode = ({ node }: TreeNodeProps): JSX.Element => {
   return (
     <div className={styles.container}>
+      {/* <StyledTreeItem
+        nodeId={String(node.id)}
+        onClick={() => console.log(node.name)}
+        labelText={node.name}
+        labelIcon={SupervisorAccountIcon}
+        labelInfo={node.children?.length}
+        color="#1a73e8"
+        bgColor="#e8f0fe"
+      >
+        {node.children && <Tree treeData={node.children} />}
+      </StyledTreeItem> */}
       <TreeItem nodeId={String(node.id)} label={node.name} onClick={() => console.log(node.name)}>
         {node.children && <Tree treeData={node.children} />}
       </TreeItem>
