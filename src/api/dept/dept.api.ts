09d4af7008fb2a3d2192ada74c065a1625033bd9
Zykov Artem <skaterpunisher@gmail.com>
2023-04-18 22:35:44 +0300

    fix dept.api

--- src/api/dept/dept.api.ts
+++ src/api/dept/dept.api.ts
@@ -1,56 +1,44 @@
-import {createApi} from '@reduxjs/toolkit/dist/query/react';
+import { createApi } from '@reduxjs/toolkit/dist/query/react';
 import { baseQueryWithReauth } from '../base/base.api';
 import { Dept } from '@/domain/model/dept/dept';
 import { DeptDetails } from '@/domain/model/dept/deptDetails';
 import { CreateDeptRequest } from './request/createDeptRequest';
-import { UserDetails } from '@/domain/model/user/userDetails';
-import { CreateOwnerRequest } from '../user/request/CreateOwnerRequest';
 import { BaseResponse } from '@/domain/model/base/baseResponse';
 
 export const deptApi = createApi({
-    reducerPath: 'DeptApi',
-    baseQuery: baseQueryWithReauth,
-    tagTypes: ['Dept'],
-    endpoints: (build) => ({
+  reducerPath: 'DeptApi',
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ['Dept'],
+  endpoints: (build) => ({
+    /**
+     * Получение поддерева отделов текущего пользователя
+     */
+    getAuthSubtree: build.query<
+      BaseResponse<Dept[]>,
+      { authId: number | undefined }
+    >({
+      query: (authId) => {
+        return {
+          method: 'POST',
+          url: '/dept/auth_subtree',
+          body: authId,
+        };
+      },
+      providesTags: ['Dept'],
+    }),
 
-        /**
-         * Получение поддерева отделов текущего пользователя
-         */
-        getAuthSubtree: build.query<BaseResponse<Dept[]>, {authId: number | undefined}>({
-            query: (authId) => {
-                return {
-                    method: 'POST',
-                    url: '/dept/auth_subtree',
-                    body: authId
-                }
-            },
-            providesTags: ['Dept']
-        }),
-
-        /**
-         * Создание нового отдела
-         */
-        getProfiles: build.mutation<DeptDetails, CreateDeptRequest>({
-            query: () => {
-                return {
-                    method: 'POST',
-                    url: '/dept/create',
-                    body: {}
-                }
-            },
-            invalidatesTags: ['Dept']
-        }),
-
-        createOwner: build.mutation<UserDetails, CreateOwnerRequest>({
-            query: (request) => {
-                return {
-                    method: 'POST',
-                    url: '/user/create_owner',
-                    body: request
-                }
-            },
-            invalidatesTags: ['Dept']
-        }),
-
-    })
-})
+    /**
+     * Создание нового отдела
+     */
+    getProfiles: build.mutation<DeptDetails, CreateDeptRequest>({
+      query: () => {
+        return {
+          method: 'POST',
+          url: '/dept/create',
+          body: {},
+        };
+      },
+      invalidatesTags: ['Dept'],
+    }),
+  }),
+});
