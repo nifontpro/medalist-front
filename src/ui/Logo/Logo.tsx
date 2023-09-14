c8e9751ad5cc20551bd982544c02304a1c7b6cae
Zykov Artem <skaterpunisher@gmail.com>
2023-09-14 13:15:11 +0300

    fix Logo and add redirect on MainPage

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -2,15 +2,20 @@ import { LogoProps } from './Logo.props';
 import Link from 'next/link';
 import LogoIcon from '@/icons/logo.svg';
 import { memo } from 'react';
-import { useAppDispatch } from '@/store/hooks/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks/hooks';
 import { setSelectedTreeId } from '@/store/features/sidebar/sidebarTree.slice';
+import { RootState } from '@/store/storage/store';
 
 const Logo = ({ className }: LogoProps): JSX.Element => {
+  const { typeOfUser } = useAppSelector(
+    (state: RootState) => state.userSelection
+  );
+
   const dispatch = useAppDispatch();
 
   return (
     <Link
-      href='/'
+      href={`department/${typeOfUser?.dept.id}`}
       className={className}
       onClick={() => dispatch(setSelectedTreeId(''))}
     >
