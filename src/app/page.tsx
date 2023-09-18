31dd796110b89c970ab8f2d39918530f3cd15b54
Zykov Artem <skaterpunisher@gmail.com>
2023-09-18 14:52:12 +0300

    [add] redirect on MainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 import Main from './_components/Main/Main';
-// import { redirect } from 'next/navigation';
-// import { useAppSelector } from '@/store/hooks/hooks';
-// import { RootState } from '@/store/storage/store';
+
+import { redirect } from 'next/navigation';
+import { useAppSelector } from '@/store/hooks/hooks';
+import { RootState } from '@/store/storage/store';
 
 export default function Home() {
-  // const { typeOfUser } = useAppSelector(
-  //   (state: RootState) => state.userSelection
-  // );
+  const { typeOfUser } = useAppSelector(
+    (state: RootState) => state.userSelection
+  );
+
+  useEffect(() => {
+    if (typeOfUser) redirect(`department/${typeOfUser?.dept.id}`);
+  }, [typeOfUser]);
 
   // redirect(`department/${typeOfUser?.dept.id}`);
 
