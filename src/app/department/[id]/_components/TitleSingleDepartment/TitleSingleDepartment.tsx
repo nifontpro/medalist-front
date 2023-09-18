609f59f08008f82a0042d36d324d6bb14a9ac26c
Zykov Artem <skaterpunisher@gmail.com>
2023-09-18 14:45:31 +0300

    [fix] add dept on deptPage

--- src/app/department/[id]/_components/TitleSingleDepartment/TitleSingleDepartment.tsx
+++ src/app/department/[id]/_components/TitleSingleDepartment/TitleSingleDepartment.tsx
@@ -19,6 +19,7 @@ import ModalWindowWithAddEvent from '@/ui/ModalWindowWithAddEvent/ModalWindowWit
 import Button from '@/ui/Button/Button';
 import { useRouter } from 'next/navigation';
 import useOutsideClick from '@/hooks/useOutsideClick';
+import EditPanelAuthBtn from '@/ui/EditPanelAuthBtn/EditPanelAuthBtn';
 
 const TitleSingleDepartment = ({
   id,
@@ -54,12 +55,20 @@ const TitleSingleDepartment = ({
       />
 
       <div className={styles.companyDescription}>
-        <EditPanelDeptBtn
+        {/* <EditPanelDeptBtn
           onlyRemove={false}
           handleRemove={deleteDepartmentAsync}
           id={department.data?.dept.id}
           getUrlEdit={getDepartmentEditUrl}
           getUrlCreate={getDepartmentCreateUrl}
+          className={styles.dots}
+        /> */}
+
+        <EditPanelAuthBtn
+          onlyRemove={false}
+          handleRemove={deleteDepartmentAsync}
+          id={department.data?.dept.id}
+          getUrlEdit={getDepartmentEditUrl}
           className={styles.dots}
         />
 
