a00b796a8ff94b3b050df3c2d8544bb30b6a7a03
Nifont <nifontbus@yandex.ru>
2023-11-20 12:51:25 +0300

    +add user event for excel

--- src/types/user/addUserReport.d.ts
+++ src/types/user/addUserReport.d.ts
@@ -1,7 +1,10 @@
-// Добавь импорты
+import {UserEvent} from "@/types/user/userEvent";
+import {ResponseError} from "@/types/base/BaseResponse";
+
 interface AddUserReport {
   userDetails: UserDetails
   success: boolean
   isUpdate: boolean
+  events: UserEvent[]
   errors: ResponseError[]
 }
---
+++ src/types/user/userEvent.d.ts
@@ -0,0 +1,7 @@
+export interface UserEvent {
+  id: number
+  userId: number
+  eventDate: number
+  eventName: string
+  isUpdate: boolean
+}
